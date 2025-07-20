147bbcc0397d817304b5bc3a51a14defc49e073f
Yash Agarwal <84071013+yash206@users.noreply.github.com>
2025-07-21 03:22:23 +0530

    Added Login and JWT functionality

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializers
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    permission_classes = [AllowAny]
